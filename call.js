66663de714f7e64605f2d478ed68778057cd47f5
ashimsharma337 <you@example.com>
2020-04-27 00:12:35 -0400

    functional page

--- call.js
+++ call.js
@@ -1,10 +1,11 @@
 var btn = document.getElementById("btn");
 var container = document.getElementById("animal-info");
+var pageCounter = 1;
 
    btn.addEventListener("click" , function(){
 
     var xhr = new XMLHttpRequest();
-    xhr.open("GET" ,"https://learnwebcode.github.io/json-example/animals-1.json");
+    xhr.open("GET" ,"https://learnwebcode.github.io/json-example/animals-" +pageCounter+'.json');
     xhr.onload = function(){
         var myData = JSON.parse(xhr.responseText);
         myHtml(myData);
@@ -12,21 +13,54 @@ var container = document.getElementById("animal-info");
     };
 
     xhr.send();
+    pageCounter++;
+    if(pageCounter>3){
+        btn.classList.add("hideme");
+    }
 
-});
-function myHtml(data){
+   });
+
+
+
+   function myHtml(data){
    var string ="";
    
    for(var i =0; i<data.length; i++){
-       string += "<p>"+"My Name is "+data[i].name+",and i am "+data[i].species+".</p>";
-   }
+
+       string += "<p>"+data[i].name+" is a "+data[i].species+" that likes to eat ";
+    
+       for(ii=0; ii<data[i].foods.likes.length; ii++){
+           if(ii==0){
+            string += data[i].foods.likes[ii]; 
+           }
+           else{
+               string += " and " + data[i].foods.likes[ii];
+           }
+       }
+       
+       for(ii=0; ii<data[i].foods.dislikes.length; ii++){
+         if(ii==0){
+         string += " and dislikes " + data[i].foods.dislikes[ii]; 
+         }
+         else{
+            string += " and " + data[i].foods.dislikes[ii];
+         }
+       }
+       
+
+    string += ".</p>";  
+
+    }
+    
+         
+
+    container.insertAdjacentHTML("beforeend" , string);
+  }
+
+   
 
 
 
-
-   container.insertAdjacentHTML("beforeend" , string);
-
-}
 
 
     
